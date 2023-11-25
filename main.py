d0b546522dc004fdf6d8d11e4e19e751f842f7fc
aigerimass <assylkhanova0@gmail.com>
2023-11-25 21:19:09 +0000

    style + ui changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-#import model
+# import model
 
 app = FastAPI(
     title="CodeSummarization"
@@ -12,24 +12,13 @@ templates = Jinja2Templates(directory="templates")
 
 # initialize model
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-
 @app.post("/", response_class=HTMLResponse)
 async def process_form(request: Request, code: str = Form(...)):
-    # Make prediction using your model
     result_summary = "mock code summarization"  # Replace with your actual prediction function
 
     return templates.TemplateResponse("result.html", {"request": request, "code": code, "result_summary": result_summary})
 
-## from fastapi import FastAPI, Request, Form
 
